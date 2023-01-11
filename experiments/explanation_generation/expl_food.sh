9cf2eaaf8d97493ab63495320ed2ae312dd47cad
Md Abdul Kadir <maktareq@gmail.com>
2023-01-11 10:27:48 +0100

    all updates

--- experiments/explanation_generation/expl_food.sh
+++ experiments/explanation_generation/expl_food.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Call this script from the experiments/explanation_generation folder as cwd.
 python ExplanationGeneration_food.py \
+		--gpu=true,
 		--input_path='/storage/rong/food-101' \
 		--save_path='/storage/rong/food-101/expl/' \
 		--model_path='../../data/plain_food.pth' \
