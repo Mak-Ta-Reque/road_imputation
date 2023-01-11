df83285cf60e9bd9e7fed5e0eabc7f9471218cb3
Md Abdul Kadir <maktareq@gmail.com>
2023-01-11 11:10:24 +0100

    test configuration added in road

--- experiments/explanation_generation/expl_food.sh
+++ experiments/explanation_generation/expl_food.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Call this script from the experiments/explanation_generation folder as cwd.
 python ExplanationGeneration_food.py \
-		--test=true
-		--gpu=true,
+		--test=true \
+		--gpu=true \
 		--input_path='/mnt/XMG4THD/abka03_data/food-101/images/food-101' \
 		--save_path='/mnt/XMG4THD/abka03_data/food-101/images/expl/' \
 		--model_path='/mnt/XMG4THD/abka03_data/food-101/weight/resnet50model.pth' \
