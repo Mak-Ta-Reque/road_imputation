1bf8c53e7ffa2930d186f5e15d8198419d45f7d5
Md Abdul Kadir <maktareq@gmail.com>
2023-01-13 07:58:51 +0100

    it working in local

--- experiments/explanation_generation/expl_food.sh
+++ experiments/explanation_generation/expl_food.sh
@@ -3,8 +3,8 @@
 python ExplanationGeneration_food.py \
 		--test=True \
 		--gpu=False \
-		--input_path='/mnt/XMG4THD/abka03_data/food-101/images/food-101' \
-		--save_path='/mnt/XMG4THD/abka03_data/food-101/images/expl/' \
-		--model_path='/mnt/XMG4THD/abka03_data/food-101/weight/resnet50model.pth' \
+		--input_path='/mnt/sda/abka03-data/food-101/images/food-101' \
+		--save_path='/mnt/sda/abka03-data/food-101/images/expl/' \
+		--model_path='//mnt/sda/abka03-data/food-101/weight/resnet50model.pth' \
 		--batch_size=20 \
-		--expl_method='gb'
+		--expl_method='gb_sg'
